2d796a41014f5a2fd5453d148ad8cf33e2e947e3
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-22 14:29:09 +0200

    adds sout to test app starting

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -14,5 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7071);
+        System.out.println("Application started on port 7071");
     }
 }
