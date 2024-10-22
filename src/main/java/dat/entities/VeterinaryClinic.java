09956d6f0791229985625b96ad42a3ea83582945
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-22 16:23:44 +0200

    adding entities

---
+++ src/main/java/dat/entities/VeterinaryClinic.java
@@ -0,0 +1,33 @@
+package dat.entities;
+
+import dat.enums.Specialization;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class VeterinaryClinic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Specialization specialization;
+
+    private String phone;
+
+    private String email;
+
+    private String address;
+
+    private String city;
+
+    private int zipCode;
+}
