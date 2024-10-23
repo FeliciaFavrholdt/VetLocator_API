a7152607184601078d1422f4e111e6b78a94f4ab
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-23 12:32:18 +0200

    adds sout for testing watchtower

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationConfig.startServer(7071);
         System.out.println("Application started on port 7071");
+        System.out.println("Adding watchtower");
     }
 }
