9babde569f20616a25995042cf55c3608838a746
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-24 11:48:20 +0200

    removes unsused code

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -14,6 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7071);
-        System.out.println("Hello World");
+        System.out.println("Application started on port 7071");
     }
 }
--- src/main/java/dat/routes/AnimalRoutes.java
+++ src/main/java/dat/routes/AnimalRoutes.java
@@ -1,6 +1,7 @@
 package dat.routes;
 
 import dat.controllers.impl.AnimalController;
+import dat.security.enums.Role;
 import io.javalin.apibuilder.EndpointGroup;
 import static io.javalin.apibuilder.ApiBuilder.*;
 
@@ -10,11 +11,11 @@ public class AnimalRoutes {
 
     protected EndpointGroup getRoutes() {
         return () -> {
-            post("/", animalController::create);          // Create a new animal
-            get("/", animalController::readAll);          // Retrieve all animals
-            get("/{id}", animalController::read);         // Retrieve specific animal by id
-            put("/{id}", animalController::update);       // Update animal details
-            delete("/{id}", animalController::delete);    // Delete specific animal
+            post("/", animalController::create, Role.USER);          // Create a new animal
+            get("/", animalController::readAll, Role.ANYONE);          // Retrieve all animals
+            get("/{id}", animalController::read, Role.ANYONE);         // Retrieve specific animal by id
+            put("/{id}", animalController::update, Role.USER);       // Update animal details
+            delete("/{id}", animalController::delete, Role.USER);    // Delete specific animal
         };
     }
 }
