c99bb1b3dffd7fd3c0e2ee66eb020525e9a28b4c
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-25 13:31:08 +0200

    adds configuration classes Roles and User to the config file

--- src/main/java/dat/config/HibernateConfig.java
+++ src/main/java/dat/config/HibernateConfig.java
@@ -2,6 +2,8 @@ package dat.config;
 
 import dat.entities.*;
 import dat.entities.Client;
+import dat.security.entities.Role;
+import dat.security.entities.User;
 import dat.utils.Utils;
 import jakarta.persistence.EntityManagerFactory;
 import org.hibernate.SessionFactory;
@@ -41,6 +43,8 @@ public class HibernateConfig {
 
     // TODO: IMPORTANT: Add Entity classes here for them to be registered with Hibernate
     private static void getAnnotationConfiguration(Configuration configuration) {
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Role.class);
         configuration.addAnnotatedClass(Client.class);
         configuration.addAnnotatedClass(Animal.class);
         configuration.addAnnotatedClass(Appointment.class);
