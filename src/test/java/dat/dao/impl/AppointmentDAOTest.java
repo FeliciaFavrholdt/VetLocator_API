b098b98700434210e167c410447d5e8106a181bc
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-28 03:47:59 +0100

    setup test files dao

---
+++ src/test/java/dat/dao/impl/AppointmentDAOTest.java
@@ -0,0 +1,46 @@
+package dat.dao.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void validatePrimaryKey() {
+    }
+}
