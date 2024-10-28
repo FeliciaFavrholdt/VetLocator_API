24dc94102c1192cddee56cc04837e9167e5c059b
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2024-10-28 03:51:56 +0100

    setup test files - controllers

---
+++ src/test/java/dat/controllers/impl/ClientControllerTest.java
@@ -0,0 +1,46 @@
+package dat.controllers.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void validatePrimaryKey() {
+    }
+
+    @Test
+    void validateEntity() {
+    }
+}
